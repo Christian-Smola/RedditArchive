32804210edee8d4921c9348cb84f619148aeda18
Christian-Smola <c.smola@yahoo.com>
2023-02-27 15:16:23 -0600

    Initial commit

---
+++ RedditArchive/RedditArchive/EnterCredentials.xaml.cs
@@ -0,0 +1,178 @@
+﻿using RestSharp;
+using System;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace RedditArchive
+{
+    /// <summary>
+    /// Interaction logic for EnterCredentials.xaml
+    /// </summary>
+    public partial class EnterCredentials : Window
+    {
+        public class Token
+        {
+            public string Access_Token;
+            public string Token_Type;
+            public string Expires_In;
+            public string Scope;
+        }
+
+        public MainWindow.Credentials CredentialsToSave = new MainWindow.Credentials();
+
+        public EnterCredentials()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterCredentials_LostFocus(object sender, RoutedEventArgs e)
+        {
+            this.Focus();
+        }
+
+        private void btnFinish_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (!TextBox_ValidationChecks())
+                return;
+
+            if (TestSQLConnection() && TestRedditAppCredentials())
+            {
+                SaveCredentials();
+            }
+
+            this.Close();
+        }
+
+        private bool TestRedditAppCredentials()
+        {
+            try
+            {
+                RestClient client = new RestClient();
+
+                RestRequest request = new RestRequest("https://www.reddit.com/api/v1/access_token", Method.Post);
+
+                request.AddHeader("Authorization", "Basic " + Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(txtAppID.Text + ":" + txtAppSecret.Text)));
+                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.AddParameter("grant_type", "password");
+                request.AddParameter("username", txtUsername.Text);
+                request.AddParameter("password", txtPassword.Text);
+
+                RestResponse response = client.Execute(request);
+
+                if (response.ResponseStatus != ResponseStatus.Completed)
+                {
+                    MessageBox.Show("Reddit credentials are incorrect");
+                    return false;
+                }
+
+                Token AppToken = JsonConvert.DeserializeObject<Token>(response.Content);
+
+                CredentialsToSave.AppID = txtAppID.Text;
+                CredentialsToSave.AppSecret = txtAppSecret.Text;
+                CredentialsToSave.Username = txtUsername.Text;
+                CredentialsToSave.Password = txtPassword.Text;
+                CredentialsToSave.AccessToken = AppToken.Access_Token;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Reddit credentials are incorrect");
+                return false;
+            }
+        }
+
+        private bool TestSQLConnection()
+        {
+            try
+            {
+                SqlConnection SQLConn = new SqlConnection("Data Source=" + txtDataSource.Text + ";Initial Catalog=" + txtDatabase.Text + ";Integrated Security=true;");
+                SQLConn.Open();
+
+                CredentialsToSave.DataSource = txtDataSource.Text;
+                CredentialsToSave.DatabaseName = txtDatabase.Text;
+
+                SQLConn.Close();
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Failed to connect to SQL database");
+                return false;
+            }
+        }
+
+        private void SaveCredentials()
+        {
+            try
+            {
+                string ProjectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+                string Path = ProjectDirectory + @"\Credentials.json";
+
+                string JsonOutput = JsonConvert.SerializeObject(CredentialsToSave);
+
+                using (StreamWriter writer = new StreamWriter(Path, true))
+                {
+                    writer.WriteLine(JsonOutput);
+                    writer.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private bool TextBox_ValidationChecks()
+        {
+            if (txtAppID.Text.Replace(" ", "").Length < 1)
+            {
+                MessageBox.Show("Please enter an App ID");
+                txtAppID.Focus();
+                return false;
+            }
+            
+            if (txtAppSecret.Text.Replace(" ", "").Length < 1)
+            {
+                MessageBox.Show("Please enter an App Secret");
+                txtAppSecret.Focus();
+                return false;
+            }
+
+            if (txtUsername.Text.Replace(" ", "").Length < 1)
+            {
+                MessageBox.Show("Please enter your reddit username");
+                txtUsername.Focus();
+                return false;
+            }
+
+            if (txtPassword.Text.Replace(" ", "").Length < 1)
+            {
+                MessageBox.Show("Please enter your reddit password");
+                txtPassword.Focus();
+                return false;
+            }
+
+            if (txtDataSource.Text.Replace(" ", "").Length < 1)
+            {
+                MessageBox.Show("Please enter your SQL data source");
+                txtDataSource.Focus();
+                return false;
+            }
+
+            if (txtDatabase.Text.Replace(" ", "").Length < 1)
+            {
+                MessageBox.Show("Please enter the SQL database you'd like to access");
+                txtDatabase.Focus();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RedditArchive/RedditArchive/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace RedditArchive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Credentials
+        {
+            public string AppID;
+            public string AppSecret;
+            public string AccessToken;
+            public string Username;
+            public string Password;
+            public string DataSource;
+            public string DatabaseName;
+
+            public Credentials(string ID, string Secret, string Token, string User, string Pass, string Source, string DBName)
+            {
+                AppID = ID;
+                AppSecret = Secret;
+                AccessToken = Token;
+                Username = User;
+                Password = Pass;
+                DataSource = Source;
+                DatabaseName = DBName;
+            }
+
+            public Credentials()
+            {
+
+            }
+        }
+
+        public class Post
+        {
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("selftext")]
+            public string Text { get; set; }
+
+            [JsonProperty("author")]
+            public string Poster { get; set; }
+
+            [JsonProperty("subreddit")]
+            public string Subreddit { get; set; }
+
+            [JsonProperty("score")]
+            public int Score { get; set; }
+
+            [JsonProperty("url")]
+            public string ContentLink { get; set; }
+
+            [JsonProperty("permalink")]
+            public string CommentLink { get; set; }
+        }
+
+        public Credentials MyCredentials;
+        public SqlConnection SQLConn;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedditArchive_IsLoaded(object sender, RoutedEventArgs e)
+        {
+            LoadCredentials();
+            EstablishSQLConn();
+            LoadPosts();
+        }
+
+        private void LoadCredentials()
+        {
+            string ProjectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            string Path = ProjectDirectory + @"\Credentials.json";
+
+            if (File.Exists(Path))
+            {
+                using (StreamReader reader = new StreamReader(Path, true))
+                {
+                    MyCredentials = JsonConvert.DeserializeObject<Credentials>(reader.ReadToEnd());
+                    reader.Close();
+                }
+            }
+            else
+            {
+                EnterCredentials Page = new EnterCredentials();
+                Page.ShowDialog();
+
+                MyCredentials = Page.CredentialsToSave;
+                MessageBox.Show(MyCredentials.Username);
+            }
+        }
+
+        private void EstablishSQLConn()
+        {
+            try
+            {
+                SQLConn = new SqlConnection("Data Source=" + MyCredentials.DataSource + ";Initial Catalog=" + MyCredentials.DatabaseName + ";Integrated Security=true;");
+                SQLConn.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Failed to connect to SQL database");
+            }
+        }
+
+        private void LoadPosts()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}

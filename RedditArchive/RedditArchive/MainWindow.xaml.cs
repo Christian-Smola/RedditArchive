856820d6cc51772baae8360d594ad7dd35426859
Christian-Smola <c.smola@yahoo.com>
2023-02-28 00:10:22 -0600

    Set up SQL queries and datagrid

--- RedditArchive/RedditArchive/MainWindow.xaml.cs
+++ RedditArchive/RedditArchive/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
+using System.Windows.Documents;
 using Newtonsoft.Json;
+using RestSharp;
 
 namespace RedditArchive
 {
@@ -38,6 +42,25 @@ namespace RedditArchive
             }
         }
 
+        public class RedditResponse
+        {
+            public string kind;
+            public RedditData data;
+        }
+
+        public class RedditData
+        {
+            [JsonProperty("children")]
+            public List<PostContainer> Containers;
+        }
+
+        public class PostContainer
+        {
+            public string kind;
+            [JsonProperty("data")]
+            public Post post;
+        }
+
         public class Post
         {
             [JsonProperty("title")]
@@ -64,6 +87,7 @@ namespace RedditArchive
 
         public Credentials MyCredentials;
         public SqlConnection SQLConn;
+        public ObservableCollection<Post> PostsToDisplay = new ObservableCollection<Post>();
 
         public MainWindow()
         {
@@ -74,6 +98,7 @@ namespace RedditArchive
         {
             LoadCredentials();
             EstablishSQLConn();
+            //ImportSavedPostsFromReddit();
             LoadPosts();
         }
 
@@ -118,9 +143,75 @@ namespace RedditArchive
         {
             try
             {
+                string strSQL = "Select * From RedditArchiveDemo";
 
+                SqlCommand cmd = new SqlCommand(strSQL, SQLConn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Post post = new Post();
+
+                    post.Title = reader.GetString(0);
+                    post.Text = reader.GetString(1);
+                    post.Poster = reader.GetString(2);
+                    post.Subreddit = reader.GetString(3);
+                    post.Score = reader.GetInt32(4);
+                    post.ContentLink = reader.GetString(5);
+                    post.CommentLink = reader.GetString(6);
+
+                    PostsToDisplay.Add(post);
+                }
+
+                MyDataGrid.ItemsSource = PostsToDisplay;
             }
             catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ImportSavedPostsFromReddit()
+        {
+            try
+            {
+                //This section pulls saved posts from reddit using reddit's rest api
+                RestClient client = new RestClient("https://oauth.reddit.com/user/watchtower32/saved.json?limit=20");
+
+                RestRequest request = new RestRequest();
+
+                request.AddHeader("Authorization", "bearer " + MyCredentials.AccessToken);
+
+                RestResponse response = client.Execute(request);
+
+                RedditResponse redditResponse = JsonConvert.DeserializeObject<RedditResponse>(response.Content);
+
+                List<Post> posts = new List<Post>();
+
+                foreach (PostContainer container in redditResponse.data.Containers)
+                    posts.Add(container.post);
+
+                //This section then saves those posts to our local SQL database
+                string strSQL = "";
+
+                foreach (Post post in posts)
+                {
+                    strSQL = "Insert Into RedditArchiveDemo(Title, Text, Poster, Subreddit, Score, ContentLink, CommentLink) Values(";
+
+                    strSQL += "'" + post.Title.Replace("'", "''") + "', ";
+                    strSQL += "'" + post.Text.Replace("'", "''") + "', ";
+                    strSQL += "'" + post.Poster.Replace("'", "''") + "', ";
+                    strSQL += "'" + post.Subreddit.Replace("'", "''") + "', ";
+                    strSQL += post.Score + ", ";
+                    strSQL += "'" + post.ContentLink.Replace("'", "''") + "', ";
+                    strSQL += "'" + post.CommentLink.Replace("'", "''") + "')";
+
+                    SqlCommand cmd = new SqlCommand(strSQL, SQLConn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
